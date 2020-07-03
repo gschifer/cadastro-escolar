f9d6db8d2bc0d8b60272a95338fe27d0cf06db10
Gabriel Schiferdecke <62288455+gschifer@users.noreply.github.com>
2020-07-03 15:52:52 -0300

    Update painel.php

--- painel.php
+++ painel.php
@@ -1,9 +1,10 @@
 <?php
-    include('verifica_login.php');
+include('verifica_login.php');
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,31 +12,33 @@
     <link rel="stylesheet" href="views/style/design.css">
     <title>Document</title>
 </head>
+
 <body>
 
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         <a href="painel.php" class="navbar-brand ml-5">Escola X</a>
         <div class="collapse navbar-collapse">
-        <ul class="navbar-nav ml-auto mr-5">
-            <li class="nav-item ">
-                <a class=" btn btn-success btn-md" href="logout.php">Sair</a>
-            </li>
-          </div>
+            <ul class="navbar-nav ml-auto mr-5">
+                <li class="nav-item ">
+                    <a class=" btn btn-success btn-md" href="logout.php">Sair</a>
+                </li>
+        </div>
     </nav>
 
     <div class=" col-md-12 container mt-5 jumbotron">
-        <h1 class="display-4">Bem vindo(a), <?php echo $_SESSION['nome'];?>!</h1>
+        <h1 class="display-4">Bem vindo(a), <?php echo $_SESSION['nome']; ?>!</h1>
         <p class="lead">
-            Clique nos botões abaixo para realizar o cadastro de um <b>aluno</b>, <b>turma</b> ou realizar a <b>inclusão</b> de um aluno dentro de uma turma. 
-            Se necessário, você pode <b>consultar</b> alunos e turmas. 
+            Clique nos botões abaixo para realizar o cadastro de um <b>aluno</b>, <b>turma</b> ou realizar a <b>inclusão</b> de um aluno dentro de uma turma.
+            Se necessário, você pode <b>consultar</b> alunos e turmas.
         </p>
         <hr class="my-4">
         <div class="d-flex justify-content-around">
-            <a class="btn btn-primary  btn-lg" href="page-cad-aluno.php" role="button">Alunos</a>              
+            <a class="btn btn-primary  btn-lg" href="page-cad-aluno.php" role="button">Alunos</a>
             <a class="btn btn-success  btn-lg" href="page-cad-turma.php" role="button">Turmas</a>
-            <a class="btn btn-warning  btn-lg" href="#" role="button">Inclusão</a>
+            <a class="btn btn-warning  btn-lg" href="vincular-page.php" role="button">Inclusão</a>
             <a class="btn btn-info  btn-lg" href="consulta.php" role="button">Consultar </a>
         </div>
     </div>
 </body>
+
 </html>
